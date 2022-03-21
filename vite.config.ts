0f4d38cf087372e07b8274b99a1f912be8c86a84
mizxinp <mizxinp@gmail.com>
2022-03-21 22:51:22 +0800

    feat: support types

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: './src/index.tsx', name: 'sparrow-ui',
